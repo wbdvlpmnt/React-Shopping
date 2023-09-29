322a3154ac5f9999f44d94041337a26b251b8d32
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-09-29 10:48:19 -0500

    Added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Carousel from "./components/Carousel";
+import Products from "./components/Products";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="bg-white text-gray-600 work-sans leading-normal text-base tracking-normal">
       <Nav />
       <Carousel />
+      <Products />
+      <About />
+      <Footer />
     </div>
   );
 }
