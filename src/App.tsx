30308a0e1ca80303944beddf788ae950caaa95cf
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-09-29 09:45:46 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1 className="p-2 m-2 text-xl text-red-300">Welcome to the store</h1>
+    </>
+  );
+}
+
+export default App;
