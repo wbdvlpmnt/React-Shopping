d08f608568557fc821315d88a800a552088468c3
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-09-29 10:37:36 -0500

    Added carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
     <div className="bg-white text-gray-600 work-sans leading-normal text-base tracking-normal">
-      <Nav/>
+      <Nav />
+      <Carousel />
     </div>
   );
 }
