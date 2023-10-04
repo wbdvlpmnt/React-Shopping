49c19949c76d42e0ac420f024795d68e447d46f2
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-10-04 10:47:43 -0500

    added items render in cart

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,7 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Fn } from "tinybench";
 import { Product } from "../app/types/types";
+import SideBarCard from "./SidebarCard";
 
 export default function Sidebar(props: {
   isOpen: boolean;
@@ -112,76 +113,7 @@ export default function Sidebar(props: {
                         role="list"
                         className="-my-6 divide-y divide-gray-200"
                       >
-                        <li className="flex py-6">
-                          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                            <img
-                              src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg"
-                              alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt."
-                              className="h-full w-full object-cover object-center"
-                            />
-                          </div>
-
-                          <div className="ml-4 flex flex-1 flex-col">
-                            <div>
-                              <div className="flex justify-between text-base font-medium text-gray-900">
-                                <h3>
-                                  <a href="#">Throwback Hip Bag</a>
-                                </h3>
-                                <p className="ml-4">$90.00</p>
-                              </div>
-                              <p className="mt-1 text-sm text-gray-500">
-                                Salmon
-                              </p>
-                            </div>
-                            <div className="flex flex-1 items-end justify-between text-sm">
-                              <p className="text-gray-500">Qty 1</p>
-
-                              <div className="flex">
-                                <button
-                                  type="button"
-                                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                                >
-                                  Remove
-                                </button>
-                              </div>
-                            </div>
-                          </div>
-                        </li>
-                        <li className="flex py-6">
-                          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                            <img
-                              src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg"
-                              alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch."
-                              className="h-full w-full object-cover object-center"
-                            />
-                          </div>
-
-                          <div className="ml-4 flex flex-1 flex-col">
-                            <div>
-                              <div className="flex justify-between text-base font-medium text-gray-900">
-                                <h3>
-                                  <a href="#">Medium Stuff Satchel</a>
-                                </h3>
-                                <p className="ml-4">$32.00</p>
-                              </div>
-                              <p className="mt-1 text-sm text-gray-500">Blue</p>
-                            </div>
-                            <div className="flex flex-1 items-end justify-between text-sm">
-                              <p className="text-gray-500">Qty 1</p>
-
-                              <div className="flex">
-                                <button
-                                  type="button"
-                                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                                >
-                                  Remove
-                                </button>
-                              </div>
-                            </div>
-                          </div>
-                        </li>
-
-                        {/* <!-- More products... --> */}
+                        <SideBarCard uniqueIds={uniqueIds} items={items} />
                       </ul>
                     </div>
                   </div>
--- src/components/SidebarCard.tsx
+++ src/components/SidebarCard.tsx
@@ -1,7 +1,51 @@
-export default function SideBarCard(props) {
-  return (
-    <>
-      <h1>sidebar card</h1>
-    </>
-  );
+import { Product } from "../app/types/types";
+
+export default function SideBarCard(props: {
+  uniqueIds: String[];
+  items: Product[];
+}) {
+  const uniqueIds = props.uniqueIds;
+  const items = props.items;
+
+  const uniqueItemsMap = uniqueIds.map((id: String, index) => {
+    const result = items.filter((item: Product) => item.id === id);
+    const quantity = result.length;
+    return (
+      <li className="flex py-6" key={index}>
+        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+          <img
+            src={result[0].url}
+            alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt."
+            className="h-full w-full object-cover object-center"
+          />
+        </div>
+
+        <div className="ml-4 flex flex-1 flex-col">
+          <div>
+            <div className="flex justify-between text-base font-medium text-gray-900">
+              <h3>
+                <a href="#">{result[0].title}</a>
+              </h3>
+              <p className="ml-4">{result[0].price}</p>
+            </div>
+            {/* <p className="mt-1 text-sm text-gray-500">Salmon</p> */}
+          </div>
+          <div className="flex flex-1 items-end justify-between text-sm">
+            <p className="text-gray-500">Qty {quantity}</p>
+
+            <div className="flex">
+              <button
+                type="button"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+      </li>
+    );
+  });
+
+  return <>{uniqueItemsMap}</>;
 }
