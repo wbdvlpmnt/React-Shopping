0bdf7cdfd0e2a4b8c87caef6a105491328e216d2
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-10-04 09:44:31 -0500

    fix tailwind errors

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -71,13 +71,13 @@ export default function Sidebar(props: {
                           className="h-6 w-6"
                           fill="none"
                           viewBox="0 0 24 24"
-                          stroke-width="1.5"
+                          strokeWidth="1.5"
                           stroke="currentColor"
                           aria-hidden="true"
                         >
                           <path
                             strokeLinecap="round"
-                            stroke-linejoin="round"
+                            strokeLinejoin="round"
                             d="M6 18L18 6M6 6l12 12"
                           />
                         </svg>
