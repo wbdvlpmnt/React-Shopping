63926514bf7de25f5e77c999165415d880d87975
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-03 15:37:06 -0500

    update the base and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/React-Shopping/",
   test: {
     globals: true,
     environment: "jsdom",
