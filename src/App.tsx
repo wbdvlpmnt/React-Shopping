114ebea2bec5161595f2f8cc1690f15ce64ff8fd
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-06 09:37:41 -0600

    update the path

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="bg-white text-gray-600 work-sans leading-normal text-base tracking-normal">
       <Router>
         <Routes>
-          <Route path="/" element={<Landing />}></Route>
+          <Route path="/React-Shopping" element={<Landing />}></Route>
         </Routes>
       </Router>
     </div>
