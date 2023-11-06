24fead6be8c8040f272e29ee2f078b8f82cc55ac
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-06 15:43:47 -0600

    update test data

--- src/app/testData.ts
+++ src/app/testData.ts
@@ -3,60 +3,63 @@ export const response = {
     {
       price: 8,
       description:
-        "Perfect connector for all types of lures, these split rings are strong, durable and lightweight. ",
+        "Ut ipsum in exercitation non amet. Esse duis aute aute ullamco ipsum proident. Proident nulla ex sunt veniam. Cillum ad pariatur officia reprehenderit ut voluptate do non consequat.",
       id: "178b7e2c20dfb6673bc53ab01d0562d5",
-      title: "Split Rings",
-      category: "Terminal tackle",
+      title: "Excepteur commodo",
+      category:
+        "Cillum aliquip ipsum consectetur et voluptate irure aliqua enim.",
       stock: 47,
       url: "https://images.unsplash.com/photo-1482440308425-276ad0f28b19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80",
     },
     {
       price: 10,
       description:
-        "Flashy metal jig perfect for casting to schooling fish. Can be retrieved at various speeds across the water column.",
+        "Adipisicing dolor occaecat veniam adipisicing eiusmod nisi sint do nisi fugiat incididunt fugiat occaecat. Laborum veniam pariatur quis pariatur laborum proident velit aliqua eiusmod eu consectetur. Quis sunt ullamco irure minim nostrud officia pariatur sit amet quis ad deserunt dolor exercitation. Culpa aute excepteur ullamco irure ad commodo reprehenderit. Consectetur irure ullamco dolore velit non dolor eiusmod occaecat consectetur consectetur. Ipsum sint qui ad reprehenderit amet proident occaecat nulla.",
       id: "1a9b60123c4ffd8d5456c8d970782167",
-      title: "Casting jig",
-      category: "Lures",
+      title: "Ex magna",
+      category:
+        "Ipsum incididunt ea deserunt sint consectetur nulla minim culpa anim elit.",
       stock: 45,
       url: "https://images.unsplash.com/photo-1583913836387-ab656f4e0457?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80",
     },
     {
       price: 10,
       description:
-        "Saltwater popper for various applications, features durable construction, tail weighted for perfect casts.",
+        "Eiusmod cillum amet ea fugiat. Aute labore ullamco mollit velit occaecat cupidatat pariatur sunt. Ex sunt in veniam enim aliquip nisi elit in.",
       id: "7d90b580f8d2957411804fffddabb09b",
-      title: "Popper",
-      category: "Lures",
+      title: "Reprehenderit incididunt",
+      category:
+        "Labore proident esse cillum aute dolore in quis cupidatat velit.",
       stock: 43,
       url: "https://images.unsplash.com/photo-1585336261022-680e295ce3fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80",
     },
     {
       price: 12,
       description:
-        "Weighted casting lure with realistic swimming action. Perfect for long casts. Upgraded with saltwater treble hooks these lures can withstand some of the biggest fish.",
+        "Dolore sit quis anim veniam veniam proident minim incididunt. Aliqua et sint consectetur ea est est occaecat aliqua et mollit quis reprehenderit. Ea magna cillum qui qui non labore deserunt. Ipsum adipisicing dolore est dolore. Tempor culpa duis velit non proident exercitation magna aliquip. Aliquip ad duis nulla sint Lorem amet ullamco consectetur tempor dolore dolore.",
       id: "885f312694ed9a4052711c50a880c50f",
-      title: "JetShark Casting Lure 30g",
-      category: "Lures",
+      title: "Quis labore",
+      category: "Ea nulla aliqua dolor ea dolor adipisicing duis irure.",
       stock: 10,
       url: "https://images.unsplash.com/photo-1467949576168-6ce8e2df4e13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
     },
     {
       price: 10,
       description:
-        "Saltwater crankbait, perfect for inshore and surf applications. Unique three hook design ensures that fish will not shake the hook after hours of finding fish. Durable saltwater grade terminal tackle included.",
+        "Labore eu anim cupidatat do adipisicing laborum elit nostrud duis in id eiusmod.",
       id: "daef733c9ae473d3f49b05743d290bb9",
-      title: "Cracnkbait",
-      category: "Lures",
+      title: "Quis ea",
+      category: "Pariatur exercitation amet excepteur do sunt adipisicing.",
       stock: 8,
       url: "https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
     },
     {
       price: 5,
       description:
-        "Extra sharp point treble hooks, constructed with durasteel technology for extended durability in the harshest environments.",
+        "Consectetur irure do occaecat anim nostrud pariatur culpa ex nisi sint commodo quis elit aute. Sit ex ipsum velit anim nostrud enim enim sunt. Quis do consectetur ut et est in irure. Ullamco adipisicing incididunt ad minim adipisicing laboris cillum.",
       id: "e3a7dbeb7cc5c2ba21a112a7a9d55f12",
-      title: "Treble Hooks",
-      category: "Terminal Tackle",
+      title: "Duis deserunt",
+      category: "Eiusmod in ullamco excepteur ipsum enim cupidatat.",
       stock: 50,
       url: "https://images.unsplash.com/photo-1550837368-6594235de85c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
     },
