b498af9c8a72ee5b1067df6bf69d55c5bd2c3f26
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-06 09:12:12 -0600

    updates for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: "/React-Shopping/",
   plugins: [react()],
-  base: "/React-Shopping/",
   test: {
     globals: true,
     environment: "jsdom",
